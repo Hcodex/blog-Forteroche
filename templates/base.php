cbef7dcfeb51e03302204bafe6223bdb5602fc8e
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-04-20 17:44:18 +0200

    Responsive features + feather-icons

--- templates/base.php
+++ templates/base.php
@@ -12,13 +12,14 @@
 
     <link href="https://fonts.googleapis.com/css2?family=Anton&display=swap" rel="stylesheet"> 
 
-    <title><?= $title ?></title>
+	<title><?= $title ?></title>
+	<script src="https://unpkg.com/feather-icons"></script>
 </head>
 
 <header>
 	<nav class="navbar fixed-top navbar-expand-lg">
 		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-			<span class="navbar-toggler-icon"></span>
+			<i data-feather="menu"></i>
 		</button>
 
 		<div class="collapse navbar-collapse" id="navbarSupportedContent">
@@ -109,6 +110,7 @@
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 <script>
+	feather.replace();
 	$(window).scroll(function(){
 		$('nav').toggleClass('scrolled', $(this).scrollTop() > 50);
 	});
