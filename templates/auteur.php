909a9dd4a71062f04c38e755ed7a17c170c2aafa
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-04-26 19:55:16 +0200

    Author description update

--- templates/auteur.php
+++ templates/auteur.php
@@ -10,9 +10,9 @@
 			<div class=" w-50"><img src="./img/jean-forteroche.jpg" class="" width="100%" alt="..."></div>
 				<div class="w-100 text-justify p-5">
 					<h2 class="text-center text-primary pb-2">Jean Forteroche</h2>
-					<p>Né à Nancy le 18 septembre 1958 Jean se passionne dès son plus jeune âge pour le théatre et l'écriture.</p>
-					<p>Il intègre le Conservatoire national d’art dramatique de Paris en 1977 et poursuit une carièrre d'acteur. Fort de son expérience de la scène il ecrit sa première pièce de théatre  "Le bal des sirènes " en 1999, suivie d'une dixaine d'autres pièces qui ne trouvèrent pas leur public. Face à cet échec il abandonne la dramaturgie et renoue avec les planches.</p>
-					<p>Pour autant, Jean a besion détancher sa soif d'écriture, en 2010 il publie son premier ouvrage qui sera plebiscité par la critique et les lecteurs et remportera le prix du livre Bruges. Ce succès marquera un tournant dans sa carrière qui compte à ce jour 10 romans suplémentaires et bientôt un 11 ème, "Billet simple pour l'Alaska" publié sous un format inédit d'épisodes en ligne.</p>
+					<p>Né à Nancy le 18 septembre 1958, Jean se passionne dès son plus jeune âge pour le théatre et l'écriture.</p>
+					<p>Il intègre le Conservatoire National d’Arts Dramatiques de Paris en 1977 et poursuit une carrière de comédien. Fort de son expérience de la scène, il écrit sa première pièce de théatre, "Le Bal des Sirènes" en 1999, suivie d'une dixaine d'autres pièces, qui ne trouvèrent pas leur public. Face à cet échec, il abandonne la dramaturgie et se lance dans la télévision. D'abord scénariste, il passe rapidement de l'autre côté de l'objectif lorsqu'un ami lui propose un petit rôle récurrent dans la série "Inspecteur Rodeau". </p>
+					<p>Pour autant, Jean a besoin d'étancher sa soif d'écriture. En 2010, il publie son premier ouvrage, qui sera plebiscité par la critique et les lecteurs et remportera le prix du livre de Bruges. Ce succès marquera un tournant dans sa carrière, qui compte à ce jour 10 romans suplémentaires... et bientôt un 11 ème, "Billet simple pour l'Alaska", publié sous un format inédit d'épisodes en ligne.</p>
 				</div>
 
 			</div>
