32fd241503d1f4065d71310c2c9dca49de3eb156
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-04-30 11:25:13 +0200

    display file name in upload form input

--- templates/base.php
+++ templates/base.php
@@ -143,6 +143,12 @@
 		$('#image_picker').modal('hide');
 	});
 
+	$('.custom-file input').change(function (e) {
+        if (e.target.files.length) {
+            $(this).next('.custom-file-label').html(e.target.files[0].name);
+        }
+    });
+
 </script>
 
 </body>
