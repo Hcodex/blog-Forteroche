7b1eab5b037d0042b56fd93f6d474753854a0740
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-05-03 12:03:55 +0200

    update form comment

--- templates/form_comment.php
+++ templates/form_comment.php
@@ -2,10 +2,12 @@
 $route = isset($post) && $post->get('id') ? 'editComment' : 'addComment';
 $submit = $route === 'addComment' ? 'Commenter' : 'Mettre à jour';
 ?>
-<h2 class="text-primary">Commenter</h2>
-<form method="post" action="../public/index.php?route=<?= $route; ?>&articleId=<?= htmlspecialchars($article->getId()); ?>">
-    <label for="content">Votre commentaire</label><br>
-    <textarea id="content" name="content"><?= isset($post) ? htmlspecialchars($post->get('content')): ''; ?></textarea><br>
-    <?= isset($errors['content']) ? $errors['content'] : ''; ?>
-    <input class="btn btn-primary" type="submit" value="<?= $submit; ?>" id="submit" name="submit">
-</form>
+<div class="container">
+    <h2 class="text-primary">Commenter</h2>
+    <form method="post" action="../public/index.php?route=<?= $route; ?>&articleId=<?= htmlspecialchars($article->getId()); ?>">
+        <label for="content">Votre commentaire</label><br>
+        <textarea id="content" name="content"><?= isset($post) ? htmlspecialchars($post->get('content')) : ''; ?></textarea><br>
+        <?= isset($errors['content']) ? $errors['content'] : ''; ?>
+        <input class="btn btn-primary" type="submit" value="<?= $submit; ?>" id="submit" name="submit">
+    </form>
+</div>
