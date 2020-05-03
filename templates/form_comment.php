f3268e0477708c5cb5980942ce01a88c933c142c
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-05-03 12:42:32 +0200

    Update form_comment.php

--- templates/form_comment.php
+++ templates/form_comment.php
@@ -1,7 +1,3 @@
-<?php
-$route = isset($post) && $post->get('id') ? 'editComment' : 'addComment';
-$submit = $route === 'addComment' ? 'Commenter' : 'Mettre à jour';
-?>
 <div class="container">
     <h2 class="text-primary">Commenter</h2>
     <form method="post" action="../public/index.php?route=<?= $route; ?>&articleId=<?= htmlspecialchars($article->getId()); ?>">
@@ -12,7 +8,6 @@ $submit = $route === 'addComment' ? 'Commenter' : 'Mettre à jour';
                 <?= isset($errors['content']) ? $errors['content'] : ''; ?>
             </div>
         </div>
-        <input class="btn btn-primary" type="submit" value="<?= $submit; ?>" id="submit" name="submit">
-
+        <input class="btn btn-primary" type="submit" value="Commenter" id="submit" name="submit">
     </form>
 </div>
