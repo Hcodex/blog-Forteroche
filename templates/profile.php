294fc19399cdff3541fb95d85c310630066caf7a
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-05-17 11:48:25 +0200

    Update profile view

--- templates/profile.php
+++ templates/profile.php
@@ -5,27 +5,50 @@
     </div>
     <section id="page-content">
         <div class="container pb-5">
-            <h2 class="text-center text-primary pb-5"><?= htmlspecialchars($this->session->get('pseudo')); ?></h2>
-            <h3 class="pt-4">Avatar</h3>
-            <?php
-            include('image_manager.php');
-            include('ajaxUpload.php');
-            ?>
+            <div class="row">
+                <div class="col-md-8 col-sm-12 order-md-last">
+                    <h2 class="text-md-center text-primary pt-1"><span class="text-muted">pseudo : </span><?= htmlspecialchars($this->session->get('pseudo')); ?></h2>
+                    <div class="row pt-md-5">
+                        <div class="col-md-6 col-sm-12 text-md-center">
+                            <h3 class="pt-4 text-muted">Email</h3>
+                            <p><?= htmlspecialchars($this->session->get('email')); ?>
+                            <a href="index.php?route=passwordModify"><i class="text-primary m-2 " data-feather="key" data-toggle="tooltip" data-placement="bottom" title="Changer le mot de passe"></i></a>
+                            </p>
+                        </div>
+                        <div class="col-md-6 col-sm-12 text-md-center">
+                            <h3 class="pt-4 text-muted">Marque page </h3>
+                            <?php
+                            if ($this->session->get('last_article_id') !== NULL) {
+                            ?>
+                                <a href="index.php?route=article&articleId=<?= $this->session->get('last_article_id'); ?>">
+                                    <i class="text-primary m-2 " data-feather="book-open"></i> Reprendre la lecture
+                                </a>
+                            <?php
+                            } else {
+                            ?>
+                                <p><i class="text-primary m-2 " data-feather="slash"></i>Non défini</p>
+                            <?php
+                            }
+                            ?>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-sm-12 col-md-4 text-md-center pt-md-1 pt-5">
+                    <h3 class=" text-muted h2">Avatar</h3>
+                    <?php
+                    include('image_manager.php');
+                    ?>
+                </div>
+            </div>
             <form method="post" action='index.php?route=editProfile'>
-                <div class="form-group">
+                <div class="form-group pt-3 pt-md-1">
                     <input type="hidden" id="picture_file_name" name="picture_file_name" placeholder="Image d' illustration" value="<?= htmlspecialchars($this->session->get('avatar')) ?>">
-                    <h3 class="pt-4">Email</h3>
-                    <p><?= htmlspecialchars($this->session->get('email')); ?></p>
-                    <a class="btn btn-primary" type="button" href="index.php?route=passwordModify" >Changer mot de passe</a>
-                    <h3 class="pt-4">Rôle</h3>
-                    <p><?= htmlspecialchars($this->session->get('role')); ?></p>
                     <div class="text-center">
                         <input type="submit" class="btn btn-primary" value="Mettre à jour" id="submit" name="submit">
                     </div>
-
             </form>
+
 
         </div>
     </section>
-    <a href="index.php">Retour à l'accueil</a>
 </main>
