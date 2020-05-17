f08f02d0cdec4d7f6d01b331b4e31e4d1beef1f7
Hcodex <61270260+Hcodex@users.noreply.github.com>
2020-05-17 11:47:45 +0200

    Update menu -> Add icons

--- templates/base.php
+++ templates/base.php
@@ -43,28 +43,34 @@
 							<?= $this->session->get('pseudo') ?>
 						</a>
 						<div class="dropdown-menu dropdown-menu-right">
-							<a class="dropdown-item text-dark" href="index.php?route=profile">Mon profil</a>
+							<a class="dropdown-item text-dark" href="index.php?route=profile">
+							<i data-feather="user" class="mx-2"></i>Mon profil
+							</a>
 							<?php
 							if ($this->session->get('last_article_id') !== NULL) {
 							?>
 								<a class="dropdown-item text-dark" href="index.php?route=article&articleId=<?= $this->session->get('last_article_id'); ?>">
-									Reprendre la lecture
+									<i data-feather="book-open" class="mx-2"></i>Reprendre la lecture
 								</a>
 							<?php
 							} else {
 							?>
 								<span data-toggle="tooltip" data-placement="bottom" title="Marque page non placé">
-									<a class="dropdown-item disabled text-muted" href="#">Reprendre la lecture</a>
+									<a class="dropdown-item disabled text-muted" href="#">
+										<i data-feather="book-open" class="mx-2"></i>Reprendre la lecture
+									</a>
 								</span>
 							<?php
 							}
 							?>
 							<?php if ($this->session->get('role') === 'admin' || $this->session->get('role') === 'corrector') { ?>
-								<a class="dropdown-item text-warning" href="index.php?route=administration">Administration</a>
+								<a class="dropdown-item text-warning" href="index.php?route=administration">
+									<i data-feather="sliders" class="mx-2"></i>Administration
+								</a>
 							<? } ?>
 							<div class="dropdown-divider"></div>
 							<a class="dropdown-item text-danger" href="index.php?route=logout">
-								<i data-feather="power"></i> Déconnexion
+								<i data-feather="power" class="mx-2"></i>Déconnexion
 							</a>
 						</div>
 					</li>
@@ -155,7 +161,7 @@
 
 	<script src="js/init.js"></script>
 	<!--<script src="https://code.jquery.com/jquery-3.5.0.min.js" integrity="sha256-xNzN2a4ltkB44Mc/Jz3pT4iU1cmeR0FkXs4pru/JxaQ=" crossorigin="anonymous"></script>-->
-	<script src="https://code.jquery.com/jquery-3.4.1.min.js" ></script>
+	<script src="https://code.jquery.com/jquery-3.4.1.min.js"></script>
 
 	<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
 	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
